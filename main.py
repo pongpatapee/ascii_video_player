136e96fe00226f07585d2fcea7dacb17c47a88a4
pongpatapee <pongpatapee1021@gmail.com>
2024-12-20 04:41:33 +0700

    feat: can play video

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
 
-ASCII_CHAR = "@#S%?*+;:,. "
-# ascii_char = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+ASCII_CHAR = ".,-~:;=!*#$@"
 
 
 def resize_img(img, new_height=100):
@@ -23,9 +22,7 @@ def image_to_ascii(img):
     for y in range(height):
         for x in range(width):
             brightness = gray_img[y, x]
-            char_index = (len(ASCII_CHAR) - 1) - round(
-                (brightness / 255) * (len(ASCII_CHAR) - 1)
-            )
+            char_index = round((brightness / 255) * (len(ASCII_CHAR) - 1))
             ascii_img += ASCII_CHAR[char_index]
             ascii_img.append(ASCII_CHAR[char_index])
         ascii_img.append("\n")
@@ -36,14 +33,40 @@ def image_to_ascii(img):
 
 
 if __name__ == "__main__":
-    img_path = "/mnt/c/Users/pongp/OneDrive/Pictures/Screenshots/Screenshot_20221026_012358.png"
+    # img_path = "/mnt/c/Users/pongp/OneDrive/Pictures/Screenshots/Screenshot_20221026_012358.png"
 
-    img = cv2.imread(img_path)
-    resized_img = resize_img(img)
-    ascii_img = image_to_ascii(resized_img)
+    # img = cv2.imread(img_path)
+    # resized_img = resize_img(img)
+    # cv2.imshow("pic", img)
+    # if cv2.waitKey(0) & 0xFF == ord("q"):
+    #     pass
+    # cv2.destroyAllWindows()
 
-    print(ascii_img)
-    print(len(ascii_img))
+    cap = cv2.VideoCapture("./data/pepe.mp4")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    if fps == 0:
+        fps = 30
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        resized_frame = resize_img(frame)
+        ascii_img = image_to_ascii(resized_frame)
+
+        print("\033[H\033[J", end="")  # clear screen
+        print(ascii_img)
+        print(len(ascii_img))
+        # cv2.imshow("camera", frame)
+
+        if cv2.waitKey(0) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
     # print(new_width)
     # print(new_height)
